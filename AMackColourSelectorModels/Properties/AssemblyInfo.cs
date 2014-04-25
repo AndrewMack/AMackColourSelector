2d062c0b1bae1df4c84e9b0fb2a87a23ce3d4a08
AndrewMack <Mr.AndrewMacKenzie@gmail.com>
2014-04-25 06:57:07 -0400

    The first breath of the project.
    Just getting it all started. Nothing done yet.

---
+++ AMackColourSelectorModels/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AMackColourSelectorModels")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AMackColourSelectorModels")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("39e9a9b9-20a5-4879-a996-1b34c5ffdfb2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ readme.txt
@@ -0,0 +1,5 @@
+AMackColourSelector is going to be an easy-to-use and easy-to-implement colour selector.
+
+AMackColourSelector is built using WPF and C# and requires .NET version 4.0+ to be installed. Eventually I would love to replicate in C++ but for now, this requirement will remain.
+
+Supports custom colours and a quick-select window (single-click to select your custom colours).
